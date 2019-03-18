f5f03ba38247cf1c0cf700aefc0671bc691cd5b5
George Dobrin <georgedobrin11@gmail.com>
2019-03-18 17:27:07 +0200

    refactoring + started development for new version

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,43 +1,22 @@
 $('document').ready(function(){
     $('.carousel').carousel({
-        interval: 2500
+        interval: 7000
       })
 })
 
-/*Sticky navbar*/
 
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+$("#mob-instagram-nav").on('click', function(e) {
+    $('html, body').animate({
+        scrollTop: $("#instagram").offset().top
+    }, 500,);
+});
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+$("#visit-card-nav").on('click', function(e) {
+    $('html, body').animate({
+        scrollTop: $("#instagram").offset().top
+    }, 500,);
+});
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
-for(let i=0; i<=9;i++){
-    $("#nav"+i).on('click', function(e) {
-        e.preventDefault();
-
-        $( '#navbar .navbar' ).find( 'a.active' ).removeClass( 'active' );
-        if(i!=0){
-            $(this).addClass( "active" );
-        }
-        
-
-        $('html, body').animate({
-            scrollTop: $("#section"+i).offset().top -66
-        }, 500,);
-    });
-}
 
 
